fa97bf3e0578f6d3dc9aa2fb23c73dc398460170
Jose Luis Ceballos <106112697+Joshua030@users.noreply.github.com>
2024-02-23 01:40:55 +0100

    Initial commit

---
+++ src/App/bootstrap.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+// without autoload
+// include __DIR__ . "/../Framework/App.php";
+
+// With autoload
+require __DIR__ ."/../../vendor/autoload.php";
+
+
+use Framework\App;
+
+$app = new App();
+
+return $app;
